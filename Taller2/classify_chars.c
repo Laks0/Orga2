456a4e16152374748d9ea9ffa1aed628a017504f
tobi <clinux01@pc62.lab1109>
2023-08-23 20:07:51 -0300

    classify q no funciona

--- Taller2/classify_chars.c
+++ Taller2/classify_chars.c
@@ -4,8 +4,34 @@
 #include <string.h>
 
 
-void classify_chars_in_string(char* string, char** vowels_and_cons) {
+int es_vocal(char ch){
+    return ch == 'a' || ch =='e' || ch == 'i' || ch == 'o' || ch == 'u'; 
+}
+
+void classify_chars_in_string(char* string, char** vowels_and_cons){
+    int i = 0;
+    char current = string[0];
+
+    int ultimaVocal = 0;
+    int ultimaCons = 0;
+
+    while (current != "\0") {
+        if (es_vocal(current)) {
+            vowels_and_cons[0][ultimaVocal] = current;
+            ultimaVocal++;
+        } else {
+            vowels_and_cons[1][ultimaVocal] = current;
+            ultimaVocal++;
+        }
+        i++;
+        current = string[i];
+    }
 }
 
 void classify_chars(classifier_t* array, uint64_t size_of_array) {
+    for (int i = 0; i < size_of_array; i++) {
+        array[i].vowels_and_consonants = calloc(2, 64);
+
+        classify_chars_in_string(array[i].string, array[i].vowels_and_consonants);
+    }
 }
