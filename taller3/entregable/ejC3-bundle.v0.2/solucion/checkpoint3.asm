ccd19479d1c4a84b7ecbc955e9543ede7d3a5f90
laks0 <laksjoaquin@gmail.com>
2023-08-30 20:08:49 -0300

    Checkpoint 3

--- taller3/entregable/ejC3-bundle.v0.2/solucion/checkpoint3.asm
+++ taller3/entregable/ejC3-bundle.v0.2/solucion/checkpoint3.asm
@@ -2,11 +2,11 @@
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar:
-;NODO_LENGTH	EQU	??
-;LONGITUD_OFFSET	EQU	??
+NODO_LENGTH	EQU	8+1+7+8+4+4
+LONGITUD_OFFSET	EQU	8+1+7+8
 
-;PACKED_NODO_LENGTH	EQU	??
-;PACKED_LONGITUD_OFFSET	EQU	??
+PACKED_NODO_LENGTH	EQU	8+1+8+4
+PACKED_LONGITUD_OFFSET	EQU	8+1+8
 
 ;########### SECCION DE DATOS
 section .data
@@ -20,12 +20,51 @@ global cantidad_total_de_elementos_packed
 
 ;########### DEFINICION DE FUNCIONES
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
-;registros: lista[?]
+;registros: lista[rdi]
 cantidad_total_de_elementos:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 8
+
+	xor rax, rax
+
+	mov rbx, [rdi]
+	xor rbp, rbp
+
+	.ciclo:
+	cmp rbx, rbp
+	je .fin
+	add eax, [rbx + LONGITUD_OFFSET]
+	mov rbx, [rbx]
+	jmp .ciclo
+
+	.fin:
+	add rsp, 8
+	pop rbp
 	ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
 ;registros: lista[?]
 cantidad_total_de_elementos_packed:
+push rbp
+	mov rbp, rsp
+	sub rsp, 8
+
+	xor rax, rax
+
+	mov rbx, [rdi]
+	xor rbp, rbp
+
+	.ciclo:
+	cmp rbx, rbp
+	je .fin
+	add eax, [rbx + PACKED_LONGITUD_OFFSET]
+	mov rbx, [rbx]
+	jmp .ciclo
+
+	.fin:
+	add rsp, 8
+	pop rbp
+	ret
 	ret
 
--- taller3/entregable/ejC3-bundle.v0.2/solucion/main.c
+++ taller3/entregable/ejC3-bundle.v0.2/solucion/main.c
@@ -22,7 +22,65 @@ int main (void){
 	product_9_f(res2, 1, 2, 3, 3, 2, 1, 2, 3, 3, 2, 3, 2, 1, 2, 3, 3, 3, 2);
 	assert(*res2 == 839808);
 	free(res2);
-	return 0;    
+
+	// Checkpoint 3
+
+	lista_t* lista = malloc(sizeof(lista_t));
+	nodo_t* nodo1 = malloc(sizeof(nodo_t));
+	nodo1->categoria = 0;
+	nodo1->arreglo = NULL;
+	nodo1->longitud = 8;
+
+	nodo_t* nodo2 = malloc(sizeof(nodo_t));
+	nodo2->categoria = 0;
+	nodo2->arreglo = NULL;
+	nodo2->longitud = 5;
+
+	nodo1->next = nodo2;
+	nodo2->next = NULL;
+
+	lista->head = nodo1;
+
+	uint32_t cant = cantidad_total_de_elementos(lista);
+
+	assert(cant == 13);
+
+	free(nodo1);
+	free(nodo2);
+	free(lista);
+
+	packed_lista_t* plista = malloc(sizeof(packed_lista_t));
+	packed_nodo_t* pnodo1 = malloc(sizeof(packed_nodo_t));
+	pnodo1->categoria = 0;
+	pnodo1->arreglo = NULL;
+	pnodo1->longitud = 8;
+
+	packed_nodo_t* pnodo2 = malloc(sizeof(packed_nodo_t));
+	pnodo2->categoria = 0;
+	pnodo2->arreglo = NULL;
+	pnodo2->longitud = 5;
+
+	packed_nodo_t* pnodo3 = malloc(sizeof(packed_nodo_t));
+	pnodo3->categoria = 0;
+	pnodo3->arreglo = NULL;
+	pnodo3->longitud = 2;
+
+	pnodo1->next = pnodo2;
+	pnodo2->next = pnodo3;
+	pnodo3->next = NULL;
+
+	plista->head = pnodo1;
+
+	cant = cantidad_total_de_elementos_packed(plista);
+
+	assert(cant == 15);
+
+	free(pnodo1);
+	free(pnodo2);
+	free(pnodo3);
+	free(plista);
+
+	return 0;
 }
 
 
