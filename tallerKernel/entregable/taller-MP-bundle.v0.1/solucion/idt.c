99c2d01bb0f16938a7a72116a4c6a98e1470cafa
tmoraut07 <tobiasmoraut7@gmail.com>
2023-10-25 00:36:02 -0300

    super print

--- tallerKernel/entregable/taller-MP-bundle.v0.1/solucion/idt.c
+++ tallerKernel/entregable/taller-MP-bundle.v0.1/solucion/idt.c
@@ -39,26 +39,26 @@ idt_descriptor_t IDT_DESC = {sizeof(idt) - 1, (uint32_t)&idt};
 /* COMPLETAR: Dado un numero de de interrupcion asigna a `idt` la entrada
  * correspondiente con nivel 0 */
 #define IDT_ENTRY0(numero)                                                     \
-  idt[numero] = (idt_entry_t) {                                                \
-    .offset_31_16 = HIGH_16_BITS(&_isr##numero),                               \
+ /*  idt[numero] = (idt_entry_t) {                                                \
+     .offset_31_16 = HIGH_16_BITS(&_isr##numero),                               \
     .offset_15_0 = LOW_16_BITS(&_isr##numero),                                 \
     .segsel = GDT_CODE_0_SEL,                                                  \
     .type = b01110,                                                               \
     .dpl = 0,                                                                  \
-    .present = 1                                                               \
-  }
+    .present = 1                                                                \
+  } */
 
 /* COMPLETAR: Dado un numero de de interrupcion asigna a `idt` la entrada
  * correspondiente con nivel 3 */
 #define IDT_ENTRY3(numero)                                                     \
-  idt[numero] = (idt_entry_t) {                                                \
-    .offset_31_16 = HIGH_16_BITS(&_isr##numero),                               \
+/*   idt[numero] = (idt_entry_t) {                                                \
+     .offset_31_16 = HIGH_16_BITS(&_isr##numero),                               \
     .offset_15_0 = LOW_16_BITS(&_isr##numero),                                 \
     .segsel = GDT_CODE_3_SEL,                                                  \
     .type = b01110,                                                               \
     .dpl = 3,                                                                  \
-    .present = 1                                                               \
-  }
+    .present = 1  *                                                              \
+  } */
 
 void idt_init() {
   // Excepciones
